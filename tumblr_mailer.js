ba396c55132fe3d336459e5074e0e84bfe51044b
Yustynn <yustynn@gmail.com>
2015-10-24 21:31:18 +0800

    Finish replacing placeholders in email template

--- tumblr_mailer.js
+++ tumblr_mailer.js
@@ -1,5 +1,7 @@
 var fs = require('fs');
 
+// Return an array of rows stored in objects. Each cell is accessible
+// by the header name of its column.
 function csvParse(csvContents) {
   var rows = csvContents.split('\n');
   rows.pop(); // remove excess \n
@@ -23,3 +25,13 @@ function csvParse(csvContents) {
 
 var csvFile = fs.readFileSync('./friend_list.csv', 'utf8');
 var entries = csvParse(csvFile);
+
+var emailTemplate = fs.readFileSync('./email_template.html', 'utf8');
+
+for (var entry in entries) {
+  // replace FIRST_NAME
+  var customizedEmail = emailTemplate.replace('FIRST_NAME', entries[0]['firstName']);
+  // replace NUM_MONTHS_SINCE_CONTACT
+  customizedEmail = customizedEmail.replace('NUM_MONTHS_SINCE_CONTACT', entries[0]['numMonthsSinceContact']);
+  console.log(customizedEmail);
+}
